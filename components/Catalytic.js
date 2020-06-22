de92c20532087ea483fc11aa39272cce11c4c758
Rajanshu Ujjwal <rajanshu.ujjwal@gmail.com>
2020-06-22 22:08:16 +0530

    fix catalysts

--- components/Catalytic.js
+++ components/Catalytic.js
@@ -3,75 +3,56 @@ export default function Catalytic(){
         [
         <article>
             <div className="container mx-auto px-6 text-center">
-            <h2 className="mb-6 text-4xl font-bold text-center">
+            <h2 className="mb-6 text-4xl font-bold">
             Catalysing Systemic Change
             </h2>
-            <h3 className="my-2 text-2xl md:mx-16 lg:mx-48 mx-auto lg:mb-8">
+            <h3 className="mb-4 text-2xl md:mx-16 lg:mx-48 mx-auto lg:mb-8">
             The PIE Foundation is a Societal Platform based on a systems-thinking approach designed to catalyse social change
             </h3>
             </div>
         </article>    
         ,
-        <section className="lg:flex mx-auto text-center">
-        <div className="rounded overflow-hidden shadow-lg">
-        <div className="md:w-1/2 sm:w-1/4 mx-auto">
-        <img src="assets/platform.svg" alt="Public Goods Platform" />
-        </div>
-        <div class="px-6 pb-8">
-        <div class="font-bold text-xl mb-2">Public Goods Platform</div>
-        <p class="text-gray-700 text-sm">
-        The suite of apps providing shared infrastructure is licensed under the Platform Commons License
-        </p>
-        </div>
-        </div>
-        
-        <div class="rounded overflow-hidden shadow-lg">
-        <div className="md:w-1/2 mx-auto">
-        <img src="assets/cocreate.svg" alt="Inspiring Co-Creation" />
-        </div>
-        <div class="px-6 pb-8">
-        <div class="font-bold text-xl mb-2">Inspiring Co-Creation</div>
-        <p class="text-gray-700 text-sm">
-        Collaborations with a spirit of co-creation develop feeling of shared ownership and provides access to innovative ideas
-        </p>
-        </div>
-        </div>
-        
-        <div class="rounded overflow-hidden shadow-lg">
-        <div className="md:w-1/2 mx-auto">
-        <img src="assets/network.svg" alt="Catalysing Networks" />
-        </div>
-        <div class="px-6 pb-8">
-        <div class="font-bold text-xl mb-2">Catalysing Networks</div>
-        <p class="text-gray-700 text-sm">
-        The platform facilitates interactions between civil society,government bodies and the marketplace
-        </p>
-        </div>
-        </div>
-        
-        <div class="rounded overflow-hidden shadow-lg">
-        <div className="md:w-1/2 mx-auto">
-        <img src="assets/agency.svg" alt="Restoring Agency" />
-        </div>
-        <div class="px-6 pb-8">
-        <div class="font-bold text-xl mb-2">Restoring Agency</div>
-        <p class="text-gray-700 text-sm">
-        Transparency and data ownership allow agency to be brought back to primary producers and their collectives
-        </p>
-        </div>
-        </div>
-        
-        <div class="rounded overflow-hidden shadow-lg">
-        <div className="md:w-1/2 mx-auto">
-        <img src="assets/scale.svg" alt="Designing for Scale" />
-        </div>
-        <div class="px-6 pb-8">
-        <div class="font-bold text-xl mb-2">Designing for Scale</div>
-        <p class="text-gray-700 text-sm">
-        Intuitive app design, open access to knowledge resources and 6C model provide the basic framework for rapid & decentralized scaling
-        </p>
-        </div>
-        </div>
+        <section className="lg:flex mx-auto text-center items-baseline mb-4 mt-4">
+            <div className="lg:w-1/5 rounded overflow-hidden shadow-lg">
+                <img className="h-20 mx-auto" src="assets/platform.svg" alt="Public Goods Platform" />
+                <div class="px-6 pb-8">
+                    <h2 class="font-bold text-xl mb-2">Public Goods Platform</h2>
+                    <p class="text-gray-700 text-sm">The suite of apps providing shared infrastructure is licensed under the Platform Commons License</p>
+                </div>
+            </div>
+
+            <div className="lg:w-1/5 rounded overflow-hidden shadow-lg">
+                <img className="h-20 mx-auto" src="assets/cocreate.svg" alt="Inspiring Co-Creation" />
+                <div class="px-6 pb-8">
+                    <h2 class="font-bold text-xl mb-2">Inspiring Co-Creation</h2>
+                    <p class="text-gray-700 text-sm">Collaborations with a spirit of co-creation develop feeling of shared ownership and provides access to innovative ideas</p>
+                </div>
+            </div>
+
+            <div className="lg:w-1/5 rounded overflow-hidden shadow-lg">
+                <img className="h-20 mx-auto" src="assets/network.svg" alt="Catalysing Networks" />
+                <div class="px-6 pb-8">
+                    <h2 class="font-bold text-xl mb-2">Catalysing Networks</h2>
+                    <p class="text-gray-700 text-sm">The platform facilitates interactions between civil society,government bodies and the marketplace</p>
+                </div>
+            </div>
+
+            <div className="lg:w-1/5 rounded overflow-hidden shadow-lg">
+                <img className="h-20 mx-auto" src="assets/agency.svg" alt="Restoring Agency" />
+                <div class="px-6 pb-8">
+                    <h2 class="font-bold text-xl mb-2">Restoring Agency</h2>
+                    <p class="text-gray-700 text-sm">Transparency and data ownership allow agency to be brought back to primary producers and their collectives</p>
+                </div>
+            </div>
+
+            <div className="lg:w-1/5 rounded overflow-hidden shadow-lg">
+                <img className="h-20 mx-auto" src="assets/scale.svg" alt="Designing for Scale" />
+                <div class="px-6 pb-8">
+                    <h2 class="font-bold text-xl mb-2">Designing for Scale</h2>
+                    <p class="text-gray-700 text-sm">Intuitive app design, open access to knowledge resources and 6C model provides framework for scaling</p>
+                </div>
+            </div>
+
         </section>
         ]
         )
